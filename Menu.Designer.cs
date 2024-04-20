7e6e10448f2841fea71807fa2e3b0d71b02ab284
TheTank20 <leoholdiman7@gmail.com>
2024-04-20 11:59:18 -0400

    Public release

---
+++ Menu.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace AudioCompressor
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            FileQueue = new ListBox();
+            label1 = new Label();
+            ManualSelectFiles = new LinkLabel();
+            StartQueue = new Button();
+            FileSizeLabel = new Label();
+            MaxFileSize = new TrackBar();
+            RemoveMetadata = new CheckBox();
+            CompletionProgressBar = new ProgressBar();
+            OverallProgressLabel = new Label();
+            QualityLevelLabel = new Label();
+            FileQualityLevel = new ProgressBar();
+            OutputTextBox = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)MaxFileSize).BeginInit();
+            SuspendLayout();
+            // 
+            // FileQueue
+            // 
+            FileQueue.AllowDrop = true;
+            FileQueue.FormattingEnabled = true;
+            FileQueue.ItemHeight = 15;
+            FileQueue.Location = new Point(12, 12);
+            FileQueue.Name = "FileQueue";
+            FileQueue.SelectionMode = SelectionMode.None;
+            FileQueue.Size = new Size(300, 379);
+            FileQueue.TabIndex = 0;
+            FileQueue.DragDrop += FileQueue_DragDrop;
+            FileQueue.DragEnter += FileQueue_DragEnter;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 417);
+            label1.Margin = new Padding(3, 0, 0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(186, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Drag and drop files into the list, or";
+            // 
+            // ManualSelectFiles
+            // 
+            ManualSelectFiles.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            ManualSelectFiles.AutoSize = true;
+            ManualSelectFiles.Location = new Point(195, 417);
+            ManualSelectFiles.Margin = new Padding(0, 0, 3, 0);
+            ManualSelectFiles.Name = "ManualSelectFiles";
+            ManualSelectFiles.Size = new Size(61, 15);
+            ManualSelectFiles.TabIndex = 2;
+            ManualSelectFiles.TabStop = true;
+            ManualSelectFiles.Text = "select files";
+            ManualSelectFiles.LinkClicked += ManualSelectFiles_LinkClicked;
+            // 
+            // StartQueue
+            // 
+            StartQueue.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            StartQueue.Location = new Point(537, 409);
+            StartQueue.Name = "StartQueue";
+            StartQueue.Size = new Size(75, 23);
+            StartQueue.TabIndex = 3;
+            StartQueue.Text = "Start";
+            StartQueue.UseVisualStyleBackColor = true;
+            StartQueue.Click += StartQueue_Click;
+            // 
+            // FileSizeLabel
+            // 
+            FileSizeLabel.AutoSize = true;
+            FileSizeLabel.Location = new Point(318, 12);
+            FileSizeLabel.Name = "FileSizeLabel";
+            FileSizeLabel.Size = new Size(143, 15);
+            FileSizeLabel.TabIndex = 4;
+            FileSizeLabel.Text = "Maximum file size: XX MB";
+            // 
+            // MaxFileSize
+            // 
+            MaxFileSize.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            MaxFileSize.Location = new Point(319, 30);
+            MaxFileSize.Maximum = 50;
+            MaxFileSize.Name = "MaxFileSize";
+            MaxFileSize.Size = new Size(293, 45);
+            MaxFileSize.TabIndex = 5;
+            MaxFileSize.TickFrequency = 5;
+            MaxFileSize.Value = 5;
+            MaxFileSize.Scroll += MaxFileSize_Scroll;
+            // 
+            // RemoveMetadata
+            // 
+            RemoveMetadata.AutoSize = true;
+            RemoveMetadata.Checked = true;
+            RemoveMetadata.CheckState = CheckState.Checked;
+            RemoveMetadata.Location = new Point(318, 81);
+            RemoveMetadata.Name = "RemoveMetadata";
+            RemoveMetadata.Size = new Size(249, 19);
+            RemoveMetadata.TabIndex = 6;
+            RemoveMetadata.Text = "Remove metadata from files (ex: cover art)";
+            RemoveMetadata.UseVisualStyleBackColor = true;
+            // 
+            // CompletionProgressBar
+            // 
+            CompletionProgressBar.Enabled = false;
+            CompletionProgressBar.Location = new Point(317, 368);
+            CompletionProgressBar.Name = "CompletionProgressBar";
+            CompletionProgressBar.Size = new Size(294, 23);
+            CompletionProgressBar.TabIndex = 8;
+            // 
+            // OverallProgressLabel
+            // 
+            OverallProgressLabel.AutoSize = true;
+            OverallProgressLabel.Enabled = false;
+            OverallProgressLabel.Location = new Point(318, 350);
+            OverallProgressLabel.Name = "OverallProgressLabel";
+            OverallProgressLabel.Size = new Size(95, 15);
+            OverallProgressLabel.TabIndex = 9;
+            OverallProgressLabel.Text = "Overall progress:";
+            // 
+            // QualityLevelLabel
+            // 
+            QualityLevelLabel.AutoSize = true;
+            QualityLevelLabel.Enabled = false;
+            QualityLevelLabel.Location = new Point(317, 297);
+            QualityLevelLabel.Name = "QualityLevelLabel";
+            QualityLevelLabel.Size = new Size(209, 15);
+            QualityLevelLabel.TabIndex = 12;
+            QualityLevelLabel.Text = "File quality level (to the right is better):";
+            // 
+            // FileQualityLevel
+            // 
+            FileQualityLevel.Location = new Point(317, 315);
+            FileQualityLevel.Maximum = 320;
+            FileQualityLevel.Name = "FileQualityLevel";
+            FileQualityLevel.Size = new Size(293, 23);
+            FileQualityLevel.TabIndex = 13;
+            // 
+            // OutputTextBox
+            // 
+            OutputTextBox.BackColor = SystemColors.WindowText;
+            OutputTextBox.BorderStyle = BorderStyle.None;
+            OutputTextBox.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            OutputTextBox.ForeColor = SystemColors.Window;
+            OutputTextBox.Location = new Point(319, 106);
+            OutputTextBox.Name = "OutputTextBox";
+            OutputTextBox.ReadOnly = true;
+            OutputTextBox.Size = new Size(291, 188);
+            OutputTextBox.TabIndex = 14;
+            OutputTextBox.Text = "";
+            OutputTextBox.TextChanged += OutputTextBox_TextChanged;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(624, 441);
+            Controls.Add(OutputTextBox);
+            Controls.Add(FileQualityLevel);
+            Controls.Add(QualityLevelLabel);
+            Controls.Add(OverallProgressLabel);
+            Controls.Add(CompletionProgressBar);
+            Controls.Add(RemoveMetadata);
+            Controls.Add(MaxFileSize);
+            Controls.Add(FileSizeLabel);
+            Controls.Add(StartQueue);
+            Controls.Add(ManualSelectFiles);
+            Controls.Add(label1);
+            Controls.Add(FileQueue);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Menu";
+            Text = "Audio Compressor";
+            Load += Menu_Load;
+            ((System.ComponentModel.ISupportInitialize)MaxFileSize).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox FileQueue;
+        private Label label1;
+        private LinkLabel ManualSelectFiles;
+        private Button StartQueue;
+        private Label FileSizeLabel;
+        private TrackBar MaxFileSize;
+        private CheckBox RemoveMetadata;
+        private ProgressBar CompletionProgressBar;
+        private Label OverallProgressLabel;
+        private Label QualityLevelLabel;
+        private ProgressBar FileQualityLevel;
+        private RichTextBox OutputTextBox;
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,253 @@
+﻿using System.Diagnostics;
+// ReSharper disable LocalizableElement
+// ReSharper disable IdentifierTypo
+// ReSharper disable StringLiteralTypo
+// ReSharper disable CommentTypo
+// ReSharper disable InconsistentNaming
+
+namespace AudioCompressor;
+
+public partial class Menu : Form
+{
+    private readonly List<string> Files = [];
+
+    private readonly List<string> AcceptedFileTypes = ["*.mp3", "*.wav", "*.aiff", "*.aif", "*.flac", "*.aac", "*.ogg", "*.wma", "*.m4a", "*.alac", "*.opus"];
+
+    // This software uses libraries from the FFmpeg project under the LGPLv2.1
+    // MLA citation - FFmpeg, 5 Apr. 2024, ffmpeg.org.
+    private readonly Process FFmpeg = new()
+    {
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = "ffmpeg.exe",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true
+        }
+    };
+
+    public Menu()
+    {
+        InitializeComponent();
+    }
+
+    private void DisplayFileSize()
+    {
+        FileSizeLabel.Text = $"Maximum file size: {MaxFileSize.Value} MB";
+        if (MaxFileSize.Value == 0)
+        {
+            FileSizeLabel.Text += " (Invalid)";
+        }
+        CheckStart();
+    }
+
+    // The visible list (built-in the ListBox control) is different from the internal file list (the Files variable).
+    // The internal file list contains the file path. For the visible list, only the file name is displayed.
+    private void UpdateVisibleList()
+    {
+        FileQueue.Items.Clear();
+        foreach (string file in Files)
+        {
+            FileQueue.Items.Add(Path.GetFileName(file));
+        }
+    }
+
+    private void CheckStart()
+    {
+        if (Files.Count > 0 && MaxFileSize.Value > 0)
+        {
+            StartQueue.Enabled = true;
+        }
+        else
+        {
+            StartQueue.Enabled = false;
+        }
+    }
+
+    private void Menu_Load(object sender, EventArgs e)
+    {
+        DisplayFileSize();
+    }
+
+    private void MaxFileSize_Scroll(object sender, EventArgs e)
+    {
+        DisplayFileSize();
+    }
+
+    private void ManualSelectFiles_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+        ManualSelectFileDialog();
+    }
+
+    private void FileQueue_DragEnter(object sender, DragEventArgs e)
+    {
+        // boilerplate code to check if the dragged data is a file
+        if (e.Data != null && e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+        else
+        {
+            e.Effect = DragDropEffects.None;
+        }
+    }
+
+    private void ManualSelectFileDialog()
+    {
+        OpenFileDialog ofd = new()
+        {
+            Multiselect = true
+        };
+        string filter = string.Join(";", AcceptedFileTypes);
+        ofd.Filter = $"Audio files|{filter}";
+
+        if (ofd.ShowDialog() == DialogResult.OK)
+        {
+            foreach (string file in ofd.FileNames)
+            {
+                if (Files.Contains(file)) continue;
+                Files.Add(file);
+            }
+        }
+
+        UpdateVisibleList();
+        CheckStart();
+    }
+
+    private void FileQueue_DragDrop(object sender, DragEventArgs e)
+    {
+        if (e.Data?.GetData(DataFormats.FileDrop) is string[] files)
+        {
+            foreach (string file in files)
+            {
+                if (Files.Contains(file)) continue;
+                string ext = Path.GetExtension(file).ToLower();
+                if (AcceptedFileTypes.Contains("*" + ext))
+                {
+                    Files.Add(file);
+                }
+            }
+        }
+
+        UpdateVisibleList();
+        CheckStart();
+    }
+
+    private async void StartQueue_Click(object sender, EventArgs e)
+    {
+        OutputTextBox.Text += "Starting queue...\n\n";
+
+        // Disable all the UI elements while the queue is running
+        StartQueue.Enabled = false;
+        FileSizeLabel.Enabled = false;
+        MaxFileSize.Enabled = false;
+        RemoveMetadata.Enabled = false;
+        label1.Enabled = false;
+        ManualSelectFiles.Enabled = false;
+        QualityLevelLabel.Enabled = true;
+        FileQualityLevel.Enabled = true;
+        CompletionProgressBar.Enabled = true;
+        OverallProgressLabel.Enabled = true;
+
+        if (Files.Count == 1)
+        {
+            OutputTextBox.Text += "Single file, can't determine overall progress\n";
+            CompletionProgressBar.Style = ProgressBarStyle.Marquee;
+        }
+        else
+        {
+            OutputTextBox.Text += "Multiple files, can determine overall progress\n";
+            CompletionProgressBar.Style = ProgressBarStyle.Continuous;
+            CompletionProgressBar.Maximum = Files.Count;
+        }
+
+        foreach (string file in Files)
+        {
+            OutputTextBox.Text += $"Starting compression on {Path.GetFileName(file)}\n";
+            try
+            {
+                await CompressFile(file, MaxFileSize.Value);
+            }
+            catch (Exception ex)
+            {
+                string error = $"An error occurred while compressing {file}:\n\n{ex.Message}";
+                OutputTextBox.Text += error + "\n";
+                MessageBox.Show(error, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        OutputTextBox.Text += "All files have been compressed successfully\n\n";
+        MessageBox.Show("All files have been compressed successfully.\nYou can find the new files the same place the input files were.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        // Re-enable all the UI elements
+        StartQueue.Enabled = true;
+        FileSizeLabel.Enabled = true;
+        MaxFileSize.Enabled = true;
+        RemoveMetadata.Enabled = true;
+        label1.Enabled = true;
+        ManualSelectFiles.Enabled = true;
+        QualityLevelLabel.Enabled = false;
+        FileQualityLevel.Enabled = false;
+        CompletionProgressBar.Enabled = false;
+        OverallProgressLabel.Enabled = false;
+        CompletionProgressBar.Style = ProgressBarStyle.Continuous;
+        CompletionProgressBar.Value = 0;
+        FileQualityLevel.Value = 0;
+        Files.Clear();
+        UpdateVisibleList();
+        CheckStart();
+    }
+
+    private async Task CompressFile(string file, int fileSize, bool updateInterface = true, int kbps = 320)
+    {
+        var kbps1 = kbps;
+        Invoke(() =>
+        {
+            FileQualityLevel.Value = kbps1;
+            OutputTextBox.Text += $"Compressing {Path.GetFileName(file)} to {kbps1} kbps...\n";
+        });
+
+        string outputFile = Path.ChangeExtension(file, null) + $"-bitrate_{kbps}.mp3";
+        string additionalArgs = RemoveMetadata.Checked ? "-map 0:a -map_metadata -1" : "";
+        FFmpeg.StartInfo.Arguments = $"-i \"{file}\" -hide_banner {additionalArgs} -c:a libmp3lame -b:a {kbps}k \"{outputFile}\" -y";
+        FFmpeg.Start();
+        await FFmpeg.WaitForExitAsync();
+
+        if (FFmpeg.ExitCode != 0)
+        {
+            throw new Exception(await FFmpeg.StandardError.ReadToEndAsync());
+        }
+
+        if (!File.Exists(outputFile))
+        {
+            throw new Exception(await FFmpeg.StandardError.ReadToEndAsync());
+        }
+
+        if (new FileInfo(outputFile).Length > fileSize * 1000 * 1000)
+        {
+            Invoke(() =>
+            {
+                OutputTextBox.Text += "File size too large, retrying with lower quality...\n";
+            });
+            File.Delete(outputFile);
+            kbps -= 32;
+            await CompressFile(file, fileSize, false, kbps);
+        }
+
+        if (updateInterface)
+        {
+            Invoke(() =>
+            {
+                CompletionProgressBar.Value++;
+                OutputTextBox.Text += $"Finished compressing {Path.GetFileName(file)}\n";
+            });
+        }
+    }
+
+    // Makes the text box scroll to the bottom when new text is added
+    private void OutputTextBox_TextChanged(object sender, EventArgs e)
+    {
+        OutputTextBox.SelectionStart = OutputTextBox.Text.Length;
+        OutputTextBox.ScrollToCaret();
+    }
+}
